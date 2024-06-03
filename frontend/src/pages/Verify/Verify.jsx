fca80c9e78c2ce048b0705fd347c2705c576666c
AMAN GUPTA <128606958+Ag18012003@users.noreply.github.com>
2024-06-03 19:59:53 +0530

    message

--- frontend/src/pages/Verify/Verify.jsx
+++ frontend/src/pages/Verify/Verify.jsx
@@ -14,10 +14,7 @@ const Verify = () => {
     const verifyPayment=async () =>{
         const response=await axios.post(url+"/api/order/verify",{success,orderId});
         if (response.data.success) {
-            navigate("/myOrders");
-        }
-        else{
-            navigate("/")
+            navigate("/");
         }
     }
 
