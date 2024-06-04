8af1ee590512b26c4666ca754113c75c847c254e
AMAN GUPTA <128606958+Ag18012003@users.noreply.github.com>
2024-06-04 22:49:12 +0530

    message

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -5,7 +5,14 @@ const authMiddleWare=async(req,res,next)=>{
     if(!token){
         return res.json({success:false,message:"Not Authorized Login Again"})
     }
-    
+    try {
+        const token_decode=jwt.verify(token,process.env.JWT_SECRET);
+        req.body.userId=token_decode.id;
+        next();
+    } catch (error) {
+        console.log(error);
+        res.json({success:false,message:"Error"})
+    }
 }
 
 
